97da10cc75e25b3bc651bb2d167d7967751eed53
Andrew Huth <andrew@huth.me>
2020-12-31 07:16:19 -0800

    Document the generate function

--- src/l-system.ts
+++ src/l-system.ts
@@ -2,6 +2,15 @@ interface Rules {
   [predecessor: string]: string;
 }
 
+/**
+ * Generator that yields successive iterations of an L-system.
+ *
+ * The `axiom` is the first iteration, and the `rules` translate the symbols of each iteration into
+ * the next. Any characters not appearing in the rules will be carried over as-is to the next
+ * iteration.
+ *
+ * @see https://en.wikipedia.org/wiki/L-system
+ */
 export function* generate(axiom: string, rules: Rules): Generator<string> {
   let current = axiom;
 
