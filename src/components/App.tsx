7722148f995cd640f3134f803178b7e7e7283899
Andrew Huth <andrew@huth.me>
2020-12-31 12:09:23 -0800

    Display some sliders and a canvas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,34 @@
+import { useRef, useState } from 'react';
+
 export default function App() {
-  return <h1>hello world</h1>;
+  const [angle, setAngle] = useState(45);
+  const [iterations, setIterations] = useState(1);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  return (
+    <>
+      <header>
+        <div>
+          <label htmlFor="angle">Angle</label>
+          <input id="angle" type="range" min="10" max="60" value={angle} onChange={(event) => setAngle(Number(event.target.value))} />
+          <span>{angle}</span>
+        </div>
+        <div>
+          <label htmlFor="iterations">Iterations</label>
+          <input id="iterations" type="range" min="1" max="20" value={iterations} onChange={(event) => setIterations(Number(event.target.value))} />
+          <span>{iterations}</span>
+        </div>
+      </header>
+      <main>
+        <canvas ref={canvasRef} style={styles.canvas} />
+      </main>
+    </>
+  );
+}
+
+const styles = {
+  canvas: {
+    height: 600,
+    width: 600,
+  },
 }
