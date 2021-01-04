38e7de543d8d1ba041b02a81123cc4bfb4ff4e9f
Andrew Huth <andrew@huth.me>
2021-01-03 19:33:47 -0800

    Add slider for segment length

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,13 +8,14 @@ import interpret from '../interpreter';
 export default function App() {
   const [angle, setAngle] = useState(45);
   const [iterations, setIterations] = useState(7);
+  const [length, setLength] = useState(2);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   function handleClick() {
     if (canvasRef.current) {
       const instructions = fractalTree(iterations);
       const lines = interpret(instructions, {
-        segmentLength: 2,
+        segmentLength: length,
         startAngle: Angle.fromDegrees(90),
         startPosition: new Vec(0, -300),
         turnAngle: Angle.fromDegrees(angle),
@@ -37,6 +38,11 @@ export default function App() {
           <label htmlFor="iterations">Iterations</label>
           <input id="iterations" type="range" min="1" max="20" value={iterations} onChange={(event) => setIterations(Number(event.target.value))} />
           <span>{iterations}</span>
+        </div>
+        <div>
+          <label htmlFor="length">Length</label>
+          <input id="length" type="range" min="0.1" max="5" step="0.1" value={length} onChange={(event) => setLength(Number(event.target.value))} />
+          <span>{length}</span>
         </div>
         <button onClick={handleClick}>Draw</button>
       </header>
